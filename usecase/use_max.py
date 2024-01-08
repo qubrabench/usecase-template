ad514201ee6feed4abb7663ca794353e86daa22d
Anurudh Peduri <anurudhp97@gmail.com>
2024-01-09 00:35:31 +0100

    use new API

--- usecase/use_max.py
+++ usecase/use_max.py
@@ -1,9 +1,10 @@
 from qubrabench.algorithms.max import max
-from qubrabench.stats import QueryStats
+from qubrabench.benchmark import track_queries, named_oracle
 
 
 def max_value_of_function(it, f):
-    stats = QueryStats()
-    result = max(it, key=f, error=1e-5, stats=stats)
+    with track_queries() as tracker:
+        result = max(it, key=named_oracle("max_key")(f), error=1e-5)
+        stats = tracker.get_stats("max_key")
 
     return result, stats
